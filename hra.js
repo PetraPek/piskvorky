4a9f0d71bc9929663b1e2ad38f9d7db616b5414e
Petra Pekařová <petra.pekarova@seznam.cz>
2023-04-10 22:56:02 +0200

    Zprovoznění prvních čtyř políček.

---
+++ hra.js
@@ -0,0 +1,40 @@
+let currentPlayer = 'circle';
+
+const playerCircle = `
+<svg width="35" height="35">
+    <circle/>
+</svg>`;
+
+const playerCross = `
+<svg width="24" height="24">
+    <cross/> 
+</svg>`;
+
+const game = (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('board__field--circle');
+    currentPlayer = 'cross';
+    event.target.disabled = true;
+    document
+      .querySelector('.game__menuType')
+      .classList.remove('board__field--circle');
+    document
+      .querySelector('.game__menuType')
+      .classList.add('board__field--cross');
+  } else {
+    event.target.classList.add('board__field--cross');
+    currentPlayer = 'circle';
+    event.target.disabled = true;
+    document
+      .querySelector('.game__menuType')
+      .classList.remove('board__field--cross');
+    document
+      .querySelector('.game__menuType')
+      .classList.add('board__field--circle');
+  }
+};
+
+document.querySelector('button:nth-child(1)').addEventListener('click', game);
+document.querySelector('button:nth-child(2)').addEventListener('click', game);
+document.querySelector('button:nth-child(3)').addEventListener('click', game);
+document.querySelector('button:nth-child(4)').addEventListener('click', game);
